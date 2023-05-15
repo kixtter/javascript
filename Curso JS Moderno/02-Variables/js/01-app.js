fb359322631b0525f69ea820059d74327929defe
Yavila <kixtersama@gmail.com>
2023-05-15 15:48:59 -0600

    02 - Variables: uso de variables

---
+++ Curso JS Moderno/02-Variables/js/01-app.js
@@ -0,0 +1,35 @@
+//Inicializar una variable con un valor
+var producto = "Monitor de 24 Pulgadas";
+console.log(producto);
+
+//Las variables se pueden reasignar
+producto = 'Monitor de 19 Pulgadas';
+console.log(producto);
+
+//JavaScript es un lenguaje de tipo dinámico, no se específica tipo de dato
+producto = 20;
+console.log(producto);
+
+//Se pueden inicializar sin valor y asignalo después
+var disponible;
+disponible = true;
+
+disponible = false;
+
+//Inicializar múltiples variables
+var categoria = "Computadoras",
+    marca = "Marca Famosa",
+    calificacion = 5;
+
+//Las variables no pueden iniciar con números
+// var 99dias;
+var dias99;
+
+// var 01_;
+var _01;
+var _usuario;
+var __usuario;
+
+
+var nombreProductoCategoriaPrecio;
+var nombre_producto_categoria_precio;
---
+++ Curso JS Moderno/02-Variables/js/03-app.js
@@ -0,0 +1,9 @@
+const producto = "Tablet";
+
+//const no puede ser reasignado
+// producto = "Monitor";
+
+console.log(producto);
+
+const precio = 20;
+console.log(precio);
