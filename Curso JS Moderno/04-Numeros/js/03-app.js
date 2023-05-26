64ede6d7f362d2a0d4598242a8f50c8d78cbf95d
Yavila <kixtersama@gmail.com>
2023-05-26 15:23:47 -0600

    Número en JavaScript

---
+++ Curso JS Moderno/04-Numeros/js/03-app.js
@@ -0,0 +1,39 @@
+let resultado;
+
+//PI
+resultado = Math.PI;
+
+//Redondear
+resultado = Math.round(2.8);
+resultado = Math.round(2.2);
+resultado = Math.round(2.6);
+resultado = Math.round(2.5);
+resultado = Math.round(2.4);
+
+//Redondear hacia arriba de forma forzada
+resultado = Math.ceil(2.1);
+
+//Redondear hacia abajo de forma forzada
+resultado = Math.floor(2.9);
+
+//Raiz
+resultado = Math.sqrt(144);
+
+//Absoluto
+resultado = Math.abs(-500);
+
+//Potencia
+resultado = Math.pow(8, 3);
+
+//Mínimo
+resultado = Math.min(3, 2, 1, 12, -3);
+
+//Máximo
+resultado = Math.max(3, 2, 1, 12, -3);
+
+//Aleatorio
+resultado = Math.random() * 20;
+
+//Aleatorio dentro de un rango
+resultado = Math.floor(Math.random() * 30);
+console.log(resultado);
---
+++ Curso JS Moderno/04-Numeros/js/06-app.js
@@ -0,0 +1,14 @@
+const numero1 = "20";
+const numero2 = "20.2";
+const numero3 = "Uno";
+const numero4 = 20;
+
+console.log(Number.parseInt(numero1));
+console.log(Number.parseFloat(numero2));
+console.log(Number.parseInt(numero3));
+
+//Revisa si un número es entero o no
+console.log(Number.isInteger(numero4));
+console.log(Number.isInteger(numero3));
+console.log(Number.isInteger(numero2));
+console.log(Number.isInteger(numero1));
