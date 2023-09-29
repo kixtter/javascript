041279472fd01de3dff0a6fdb3c89ef39a9e7287
Yavila <kixtersama@gmail.com>
2023-09-29 16:31:31 -0600

    Aprendiendo Eventos 2 LENOVO

---
+++ Curso JS Moderno/14-Eventos/js/04-scripts.js
@@ -0,0 +1,11 @@
+const formulario = document.querySelector("#formulario");
+
+formulario.addEventListener("submit", validarFormulario);
+
+function validarFormulario(e)
+{
+    e.preventDefault();
+
+    console.log("Buscando");
+    console.log(e.target.action);
+}
