dbee18750f7383510189fabffeca1f75f723380c
Yavila <kixtersama@gmail.com>
2023-10-20 16:34:50 -0600

    Proyecto 3 Buscador(En Proceso) LENOVO

---
+++ Curso JS Moderno/17-PROYECTO-Buscador/js/app.js
@@ -0,0 +1,163 @@
+//VARIABLES
+const marca = document.querySelector('#marca');
+const year = document.querySelector('#year');
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
+const color = document.querySelector('#color');
+
+//Contenedor para los resultados
+const resultado = document.querySelector('#resultado');
+
+const max = new Date().getFullYear();
+const min = new Date().getFullYear() - 10;
+
+//Generar un objeto con la busqueda
+const datosBusqueda =
+{
+    marca: '',
+    year: '',
+    minimo: '',
+    maximo: '',
+    puertas: '',
+    transmision: '',
+    color: '',
+}
+
+//EVENTOS
+document.addEventListener('DOMContentLoaded', () =>
+{
+    mostrarAutos(autos);//Muestra los autos al cargar
+
+    //Llena las opciones de años
+    llenarSelect();
+});
+
+//Event listener para los select de búsqueda
+marca.addEventListener('change', e =>
+{
+    datosBusqueda.marca = e.target.value;
+    filtrarAuto();
+});
+
+year.addEventListener('change', e =>
+{
+    datosBusqueda.year = parseInt(e.target.value);
+    filtrarAuto();
+});
+
+minimo.addEventListener('change', e =>
+{
+    datosBusqueda.minimo = parseInt(e.target.value);
+    filtrarAuto();
+});
+
+maximo.addEventListener('change', e =>
+{
+    datosBusqueda.maximo = parseInt(e.target.value);
+    filtrarAuto();
+});
+
+puertas.addEventListener('change', e =>
+{
+    datosBusqueda.puertas = e.target.value;
+});
+
+transmision.addEventListener('change', e =>
+{
+    datosBusqueda.transmision = e.target.value;
+});
+
+color.addEventListener('change', e =>
+{
+    datosBusqueda.color = e.target.value;
+});
+
+//FUNCIONES
+function mostrarAutos(autos)
+{
+    limpiarHTML();//Elimina el HTML previo
+
+    autos.forEach(auto =>
+    {
+        const {marca, modelo, year, puertas, transmision, precio, color} = auto;
+        const autoHTML = document.createElement('p');
+        autoHTML.textContent = `
+            ${marca} ${modelo} - ${year} - ${puertas} PUERTAS - TRANSMISIÓN: ${transmision} - PRECIO: ${precio} - COLOR: ${color}
+        `;
+
+        resultado.appendChild(autoHTML);
+    });
+}
+
+//Limpiar HTML
+function limpiarHTML()
+{
+    while (resultado.firstChild)
+    {
+        resultado.removeChild(resultado.firstChild);
+    }
+}
+
+//Genera los años del select
+function llenarSelect()
+{
+    for (let i = max; i >= min; i--)
+    {
+        const opcion = document.createElement('option');
+        opcion.value = i;
+        opcion.textContent = i;
+        year.appendChild(opcion);
+    }
+}
+
+//Función que filtra en base a la búsqueda
+function filtrarAuto()
+{
+    const resultado = autos.filter(filtrarMarca).filter(filtrarYear).filter(filtrarMinimo).filter(filtrarMaximo);
+    // console.log(resultado);
+    mostrarAutos(resultado);
+}
+
+function filtrarMarca(auto)
+{
+    const {marca} = datosBusqueda;
+    if (marca) {
+        return auto.marca === marca;
+    }
+
+    return auto;
+}
+
+//Cuidado con los tipos de datos, en la colección de datos es int y en la búsqueda es un string
+//Recordar los tipos de datos que se estan manejando y hacer las conversiones correspondientes
+function filtrarYear(auto)
+{
+    const {year} = datosBusqueda;
+    if (year) {
+        return auto.year === year;
+    }
+
+    return auto;
+}
+
+function filtrarMinimo(auto)
+{
+    const {minimo} = datosBusqueda;
+    if (minimo) {
+        return auto.precio >= minimo;
+    }
+
+    return auto;
+}
+
+function filtrarMaximo(auto)
+{
+    const {maximo} = datosBusqueda;
+    if (maximo) {
+        return auto.precio <= maximo;
+    }
+
+    return auto;
+}
